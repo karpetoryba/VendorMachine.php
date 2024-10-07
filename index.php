af1778baf36a5ef7c47e474df7c8d9d22be4e88c
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-07 14:28:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+class VendorMachine {
+    private $isOn;
+    private $snacksQty;
+    private $money;
+
+
+    public function __construct() {
+        $this->isOn = false;
+        $this->snacksQty = 50;
+        $this->money = 0;
+    }
+    
+ public function buySnack(): void {
+    $this->isOn = true;
+
+    if ($this->snacksQty === 0) {
+        throw new Exception("Plus de snacks");
+    }
+    $this->snacksQty -= 1;
+    $this->money += 2;
+}
+
+public function reset(): void {
+    $this->isOn = false;
+
+    if (this->snacksQty > 50) {
+      throw new Exception("la machine est deja rempli");
+    }
+    $this->snacksQty = $this->calculateLeftSnackQty();
+
+    $this->money = 0;
+    $this->isOn = true;
+  }
+
+  private function calculateLeftSnackQty() {
+    return $this->snacksQty + (50 - $this->snacksQty);
+}
+
+  public function shootWithFoot(): void {
+    $this->isOn = false;
+
+    $this->dropMoney();
+    $this->dropSnacks();
+
+    echo "Snacks tombés : {$this->snacksQty} et monnaie tombée : {$this->money}";
+}
+
+
+
+  private function dropMoney() {
+    $moneyToDrop = 20;
+    if ($this->money < 20) {
+        $moneyToDrop = $this->money;
+    }
+    $this->money = $this->money - $moneyToDrop;
+}
+
+private function dropSnacks() {
+     $snacksQtyToDrop = 5;
+    if ($this->snacksQty < 5) {
+      $snacksQtyToDrop = $this->snacksQty;
+    }
+    $this->snacksQty = $this->snacksQty - $snacksQtyToDrop;
+  }
+}
+
+$machine = new VendorMachine();
+$machine->shootWithFoot();

f3262be767e54cf6a8a8bcbe5ce582beb124e2e6
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-09 09:07:07 +0200

    fixe

--- view/home.php
+++ view/home.php
@@ -29,19 +29,7 @@
         <button type="submit">Ajouter</button>
     </form>
 
-    <h2>Entrer l'adresse de livraison</h2>
-    <form action="../controller/delivery-destination.php" method="POST">
-        <label for="shippingAddress">Adresse :</label><br>
-        <input type="text" id="shippingAddress" name="shippingAddress" required><br><br>
 
-        <label for="shippingCity">Ville :</label><br>
-        <input type="text" id="shippingCity" name="shippingCity" required><br><br>
-
-        <label for="shippingCountry">Pays :</label><br>
-        <input type="text" id="shippingCountry" name="shippingCountry" required><br><br>
-
-        <input type="submit" value="Valider l'adresse">
-    </form>
 </main>
 </body>
 </html>
--- view/order-created.php
+++ view/order-created.php
@@ -10,7 +10,19 @@
 	</header>
 	
 	<main>
-		<p>La commande a été créée avec succès</p>
+    <h2>Entrer l'adresse de livraison</h2>
+    <form action="../controller/delivery-destination.php" method="POST">
+        <label for="shippingAddress">Adresse :</label><br>
+        <input type="text" id="shippingAddress" name="shippingAddress" required><br><br>
+
+        <label for="shippingCity">Ville :</label><br>
+        <input type="text" id="shippingCity" name="shippingCity" required><br><br>
+
+        <label for="shippingCountry">Pays :</label><br>
+        <input type="text" id="shippingCountry" name="shippingCountry" required><br><br>
+
+        <input type="submit" value="Valider l'adresse">
+    </form>
 	</main>
 
 	</body>

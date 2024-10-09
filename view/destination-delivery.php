aa9a27f744e21b38198f61e6144e0113bb855278
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-09 09:30:24 +0200

    form pour select delivery

--- view/destination-delivery.php
+++ view/destination-delivery.php
@@ -11,6 +11,25 @@
 	
 	<main>
 	<p>On vous prépare le commend pour le livraison<p>
+	<h2>choisir le mode de livraison :</h2>
+    <form action="process_delivery.php" method="post">
+        <label>
+            <input type="radio" name="delivery_method" value="Chronopost Express" required>
+            Chronopost Express
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="delivery_method" value="Point relais" required>
+            Point relais
+        </label>
+        <br>
+        <label>
+            <input type="radio" name="delivery_method" value="Domicile" required>
+            Domicile
+        </label>
+        <br><br>
+        <input type="submit" value="choisir">
+    </form>
 	</main>
 
 	</body>
